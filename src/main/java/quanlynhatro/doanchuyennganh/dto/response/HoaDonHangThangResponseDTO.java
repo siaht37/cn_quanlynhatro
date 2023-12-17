d3c88c997a3abb30f339971c832655e864205f89
Asus <thaihoang03072002@gmail.com>
2023-12-17 16:55:31 +0700

    count Phong con trong

---
+++ src/main/java/quanlynhatro/doanchuyennganh/dto/response/HoaDonHangThangResponseDTO.java
@@ -0,0 +1,21 @@
+package quanlynhatro.doanchuyennganh.dto.response;
+
+import lombok.*;
+import quanlynhatro.doanchuyennganh.entity.ChiTietPhieuThueTienIch;
+import quanlynhatro.doanchuyennganh.entity.HoaDonHangThang;
+import quanlynhatro.doanchuyennganh.entity.SoDien;
+import quanlynhatro.doanchuyennganh.entity.SoNuoc;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@ToString
+@Getter
+@Setter
+public class HoaDonHangThangResponseDTO {
+    private HoaDonHangThang hoaDonHangThang;
+    private SoDien soDien;
+    private SoNuoc soNuoc;
+    private List<ChiTietPhieuThueTienIch> chiTietPhieuThueTienIches;
+}
--- src/main/java/quanlynhatro/doanchuyennganh/service/HoaDonHangThangService.java
+++ src/main/java/quanlynhatro/doanchuyennganh/service/HoaDonHangThangService.java
@@ -2,15 +2,26 @@ package quanlynhatro.doanchuyennganh.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import quanlynhatro.doanchuyennganh.dto.response.HoaDonHangThangResponseDTO;
 import quanlynhatro.doanchuyennganh.entity.HoaDonHangThang;
+import quanlynhatro.doanchuyennganh.repository.IChiTietPhieuThueTienIchRepository;
 import quanlynhatro.doanchuyennganh.repository.IHoaDonHangThangRepository;
+import quanlynhatro.doanchuyennganh.repository.ISoDienRepository;
+import quanlynhatro.doanchuyennganh.repository.ISoNuocRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class HoaDonHangThangService implements IService<HoaDonHangThang> {
     @Autowired
     private IHoaDonHangThangRepository hoaDonHangThangRepository;
+    @Autowired
+    private ISoDienRepository soDienRepository;
+    @Autowired
+    private ISoNuocRepository soNuocRepository;
+    @Autowired
+    private IChiTietPhieuThueTienIchRepository chiTietPhieuThueTienIchRepository;
 
     @Override
     public List<HoaDonHangThang> getAll() {
@@ -25,5 +36,17 @@ public class HoaDonHangThangService implements IService<HoaDonHangThang> {
     @Override
     public HoaDonHangThang update(HoaDonHangThang hoaDonHangThang) {
         return null;
+    }
+
+    public List<HoaDonHangThangResponseDTO> insertHoaDonsTheoThang(int month) {
+        List<HoaDonHangThangResponseDTO> hoaDonHangThangResponseDTOS = new ArrayList<>();
+
+
+        for (HoaDonHangThangResponseDTO hoaDonHangThangResponseDTO : hoaDonHangThangResponseDTOS) {
+
+
+        }
+
+        return hoaDonHangThangResponseDTOS;
     }
 }
