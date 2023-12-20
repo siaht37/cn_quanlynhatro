4f2a517ce62d9e19562e43fb25c4acda2be1d98e
Asus <thaihoang03072002@gmail.com>
2023-12-20 16:30:25 +0700

    login duoc 100% nhung chua update matkhau

--- src/main/java/quanlynhatro/doanchuyennganh/DoanchuyennganhApplication.java
+++ src/main/java/quanlynhatro/doanchuyennganh/DoanchuyennganhApplication.java
@@ -2,10 +2,9 @@ package quanlynhatro.doanchuyennganh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DoanchuyennganhApplication extends SpringBootServletInitializer {
+public class DoanchuyennganhApplication {
 
     public static void main(String[] args) {
 
---
+++ src/main/java/quanlynhatro/doanchuyennganh/test.java
@@ -0,0 +1,5 @@
+package quanlynhatro.doanchuyennganh;
+
+public class test {
+
+}
